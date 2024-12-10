7a97d18672ced8197e58977032e38d3e0faa2bd4
David8060 <140398113+David8060@users.noreply.github.com>
2024-12-10 16:20:02 +0400

    test cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,8 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     // Increase timeouts
-    defaultCommandTimeout: 1900000, // Set timeout for commands (in milliseconds)
-    pageLoadTimeout: 2000000, // Set timeout for full page loads (in milliseconds)
+    defaultCommandTimeout: 600, // Set timeout for commands (in milliseconds)
+    pageLoadTimeout: 800, // Set timeout for full page loads (in milliseconds)
     video: false, 
   },
 });
